0c50836d0c5d68568dbe63a1fac0e1c341454f15
Eric Dasse <christ.gououi@gmail.com>
2024-07-14 10:56:54 +0200

    Track experiments with DVCLive

--- wine_quality_prediction_modeling/train.py
+++ wine_quality_prediction_modeling/train.py
@@ -6,6 +6,7 @@ import joblib
 from loguru import logger
 import pandas as pd
 from sklearn.linear_model import LinearRegression
+from dvclive import Live
 
 
 def train(training_data_path: Path) -> LinearRegression:
@@ -28,15 +29,19 @@ def main():
 
     prepared_data_path = Path(__file__).parent.parent / "data/prepared"
     training_data_path = prepared_data_path / "train.csv"
-    logger.info("Training the wine quality prediction model...")
-    trained_model = train(training_data_path)
 
-    # Save model to model dir
-    model_dir = Path(__file__).parent.parent / "model"
-    model_dir.mkdir(exist_ok=True)
-    model_dump = model_dir / "wine_quality_model.joblib"
-    logger.info(f"Saving model to {model_dump}...")
-    joblib.dump(trained_model, model_dump)
+    with Live() as live:
+        logger.info("Training the wine quality prediction model...")
+        trained_model = train(training_data_path)
+
+        # Save model to model dir
+        model_dir = Path(__file__).parent.parent / "model"
+        model_dir.mkdir(exist_ok=True)
+        model_dump = model_dir / "wine_quality_model.joblib"
+        logger.info(f"Saving model to {model_dump}...")
+        joblib.dump(trained_model, model_dump)
+
+        live.log_artifact(str(model_dump), type="model")
 
     logger.success("Training successful!")
 
